00c682af205da94d1b7762eeaa141254a280bd3b
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-02-11 21:28:03 +0545

    feat: return simple json response

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"log"
 	"net/http"
 )
 
+type Hello struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(Hello{Message: "Hello, World!"})
 	})
 	mux.HandleFunc("GET /query", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Query().Encode()))
