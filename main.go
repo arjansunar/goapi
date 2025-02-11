38bdfe51018d34bafbad8d18e2b034d38cff94cc
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-02-11 22:03:12 +0545

    feat: enable generating captchas

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/mojocn/base64Captcha"
 )
 
 type Hello struct {
 	Message string `json:"message"`
+}
+
+func genCaptcha64(w http.ResponseWriter, r *http.Request) {
+	c := base64Captcha.NewCaptcha(base64Captcha.DefaultDriverDigit, base64Captcha.DefaultMemStore)
+	id, b64s, answer, err := c.Generate()
+	fmt.Printf("id: %s answer: %s", id, answer)
+	body := map[string]interface{}{"code": 1, "data": b64s, "captchaId": id, "msg": "success"}
+	if err != nil {
+		body = map[string]interface{}{"code": 0, "msg": err.Error()}
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	json.NewEncoder(w).Encode(body)
 }
 
 func main() {
@@ -17,6 +31,7 @@ func main() {
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(Hello{Message: "Hello, World!"})
 	})
+	mux.HandleFunc("GET /get-captcha", genCaptcha64)
 	mux.HandleFunc("GET /query", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Query().Encode()))
 	})
