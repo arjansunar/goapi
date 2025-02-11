31c887298daeb8c25260854fa9c8595d95289abe
arjansunar <arjan.gahatrajsunar@gmail.com>
2025-02-11 18:39:00 +0545

    feat: add default handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("GET /bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	println(`
+                                             ▄▄  
+  ▄▄█▀▀▀█▄█               ██                 ██  
+▄██▀     ▀█              ▄██▄                    
+██▀       ▀  ▄██▀██▄    ▄█▀██▄   ▀████████▄▀███  
+██          ██▀   ▀██  ▄█  ▀██     ██   ▀██  ██  
+██▄    ▀██████     ██  ████████    ██    ██  ██  
+▀██▄     ██ ██▄   ▄██ █▀      ██   ██   ▄██  ██  
+  ▀▀███████  ▀█████▀▄███▄   ▄████▄ ██████▀ ▄████▄
+                                   ██            
+                                 ▄████▄          
+    `)
+	log.Default().Println("Listening on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	print("Hello, World!")
 }
